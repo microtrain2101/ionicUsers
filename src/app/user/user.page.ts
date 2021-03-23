2b413f4fbbbe049e6616e86934f889f57dd48df1
chad svastisalee <swasgrafx@gmail.com>
2021-03-23 15:19:24 -0500

    Add the ability to delete a user

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -34,5 +34,11 @@ export class UserPage implements OnInit {
       }
     );
   }
-  
+  deleteUser(id:string): void {
+    if (confirm("Are you sure to delete " + this.user.username)) {
+      this.usersService.deleteUser(id).subscribe(
+        () => { this.router.navigate(['/users']) }
+      );
+    }
+  }
 } 
